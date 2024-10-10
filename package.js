5634f2a064da70991e887eff4d1c86d611d0afaf
Mark00420 <qa@gna.energy>
2024-10-10 17:01:14 +0530

    Create package.js

---
+++ package.js
@@ -0,0 +1,14 @@
+{
+  "name": "hello-website",
+  "version": "1.0.0",
+  "description": "A simple Node.js website saying Hello",
+  "main": "app.js",
+  "scripts": {
+    "start": "node app.js"
+  },
+  "author": "Your Name",
+  "license": "ISC",
+  "dependencies": {
+    "express": "^4.17.1"
+  }
+}
