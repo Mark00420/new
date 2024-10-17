d06c82836837ed95447bd4dc95a81a4dc81ec3ed
Mark00420 <qa@gna.energy>
2024-10-17 10:46:24 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,25 +1,12 @@
-// app.js
-
-//const express = require('express');
-//const app = express();
-//const port = 3000;
-
-//app.get('/', (req, res) => {
- // res.send('Hello, GNA!');
-//});
-
-//app.listen(port, () => {
-  //console.log(`Example app listening at http://localhost:${port}`);
-//});
 const express = require('express');
+const path = require('path');
 const app = express();
 const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  res.sendFile(path.join(__dirname, 'index.html')); // Updated to serve index.html from the root
 });
 
-app.listen(port, '0.0.0.0', () => {
-  console.log(`App listening at http://0.0.0.0:${port}`);
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
 });
-
